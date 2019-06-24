abca4ef7752527dd2c0167cb654e7f41955f89a5
aliffrhn <alif.farhanw@gmail.com>
2019-06-24 15:37:54 +0700

    minor change

--- Header.js
+++ Header.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native'
+import { Text, TextInput } from 'react-native'
 
 export default class Header extends Component{
 
     state = {
-        name : this.props.text
+        name : this.props.text,
+        usia : ''
+    }
+
+    handlerInput = event => {
+        this.setState({usia : event})
     }
 
     render(){
 
       return(
-        <Text>
+        <View>
           {this.state.text}
-        </Text>
+          <TextInput 
+            onChangeText = {this.handlerInput}
+            placeholder = "Masukan Inputan"
+            value = {this.state.usia}
+           />
+        </View>
       )
     }
   }
