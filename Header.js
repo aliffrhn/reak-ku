8d5b2d7ef5d9bb1715034bf099af08ce6564ef59
aliffrhn <alif.farhanw@gmail.com>
2019-06-24 11:15:22 +0700

    Test inline react component

--- Header.js
+++ Header.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Text } from 'react-native'
 
-export default class Header extends Component{
+export default class Header extends React.Component{
   
     render(){
       return(
